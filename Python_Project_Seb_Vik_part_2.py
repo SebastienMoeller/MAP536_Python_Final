c655079ce603f7d5faa178495eb0f4ffbfb63bf2
Viktor Malesevic <32514119+ViktorMalesevic@users.noreply.github.com>
2018-01-09 23:23:10 +0100

    Try to split the dataset into training and testing with random indexes...

--- Python_Project_Seb_Vik_part_2.py
+++ Python_Project_Seb_Vik_part_2.py
@@ -10,6 +10,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize
+import random
 #%% 
 data = pd.read_csv("C:\\Users\\DataScience\\Desktop\\Python Course\\MAP536_Python_Final-master\\data.csv")
 #data = data.drop('Unnamed: 0', 1)
@@ -164,3 +165,38 @@ print(meta_res)
 
 # WE SEE THAT MAYBE DEGREE THREE FITS BETTER THAN DEGREE 2. HOWEVER, THIS IS NORMAL: THE ERROR WILL ALWAYS DECREASE WITH MORE COMPLEX MODEL
 ## LET'S THEN TRY TO DO CROSS VALIDATION TO DECREASE THE TESTING AND TRAINING ERROR
+
+# 80% training and 20% testing
+
+data.reset_index(inplace = True)
+
+#%%
+N = len(data)
+
+randomized_indices = np.arange(N)
+# random.sample(randomized_indices, int(4 * N / 5))
+np.random.shuffle(randomized_indices)
+
+shuffled_data = data[randomized_indices] #dataset after index randomization
+
+### Split the dataset into two subsets ###
+
+## Build training set ##
+#training_data = data.sample(int(4 * N / 5), replace = 'True')
+
+## Build testing set ##
+#testing_data = data.drop(training_data.index, axis = 0)
+
+
+#%%
+### Neural network training ###
+Classifier = nn.MLPClassifier(hidden_layer_sizes = (50), activation = 'relu', solver = 'lbfgs',  max_iter = 10000000)
+Classifier.fit(training_features, training_targets)
+
+#NOTA: Here we realized that with two layers we obtain way better scores for (10,50) than for (50,10)
+
+### Neural network testing ###
+score_train = Classifier.score(training_features, training_targets)
+score_test = Classifier.score(testing_features, testing_targets)
+print ('Score on the training set : ', score_train)
+print ('Score on the testing set : ', score_test)
