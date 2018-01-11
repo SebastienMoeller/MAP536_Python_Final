ad4a70645aa2229ab3893e24885f7a1bf98ebc91
SebastienMoeller <32814091+SebastienMoeller@users.noreply.github.com>
2018-01-11 23:29:40 +0100

    Review

--- Python_Project_Seb_Vik_part_2.py
+++ Python_Project_Seb_Vik_part_2.py
@@ -10,9 +10,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.optimize
-import random
 #%% 
-data = pd.read_csv("C:\\Users\\DataScience\\Desktop\\Python Course\\MAP536_Python_Final-master\\data.csv")
+data = pd.read_csv("C:\\Users\\Sebastien\\Desktop\\MAP536_Python_Final-master\\data.csv")
 #data = data.drop('Unnamed: 0', 1)
 #%%
 del data['test']
@@ -155,35 +154,39 @@ def polynomialError(param, x, y):
 # WE SEE THAT MAYBE DEGREE THREE FITS BETTER THAN DEGREE 2. HOWEVER, THIS IS NORMAL: THE ERROR WILL ALWAYS DECREASE WITH MORE COMPLEX MODEL
 ## LET'S THEN TRY TO DO CROSS VALIDATION TO DECREASE THE TESTING AND TRAINING ERROR
 
-# 80% training and 20% testing
-
 data.reset_index(inplace = True)
 
-#%%
-N = len(data)
+# 80% training and 20% testing
+def shuff(data, ratio = 0.3):    
+    N = len(data)
 
-randomized_indices = np.arange(N)
-np.random.shuffle(randomized_indices)
+    randomized_indices = np.arange(N)
+    np.random.shuffle(randomized_indices)
  
-#dataset after index randomization
-shuffled_data = data.iloc[randomized_indices,:] 
+    #dataset after index randomization
+    shuffled_data = data.iloc[randomized_indices,:] 
 
-### Split the dataset into two subsets ###
+    ### Split the dataset into two subsets ###
 
-## Build training set ##
-training_data = shuffled_data[:int(4 * N / 5)]
+    ## Build training set ##
+    training_data = shuffled_data[:int((1-ratio)*N)]
 
-## Build testing set ##
-testing_data = shuffled_data[int(4 * N / 5):]
+    ## Build testing set ##
+    testing_data = shuffled_data[int((1-ratio)*N):]
+    
+    return training_data, testing_data
+#%%
 
 #%%
-training_data = training_data.sort_values('volume', ascending=True)    
-testing_data = testing_data.sort_values('volume', ascending=True)  
+training_data, testing_data = shuff(data)
 
-x_data = training_data['volume'] 
+training_data = training_data.sort_values('tBlockchain', ascending=True)    
+testing_data = testing_data.sort_values('tBlockchain', ascending=True)  
+
+x_data = training_data['tBlockchain'] 
 y_data = training_data['close']   
 
-x_test = testing_data['volume'] 
+x_test = testing_data['tBlockchain'] 
 y_test = testing_data['close']
 
 #%%
@@ -192,9 +195,8 @@ def magic(n, x_data = x_data, x_test = x_test, y_data = y_data, y_test = y_test)
     param0 = np.ones(n)
     param1, meta_res = scipy.optimize.leastsq(polynomialError, param0[:], args = (x_data, y_data))
     
-    y_model = polynomialModelN(n, param1, x_data)
-    plt.scatter(x_data,y_data,label='Training Data')
-    plt.scatter(x_test,y_test,label='Testing Data', color = 'Green' )
+    plt.scatter(x_data,y_data,label='Training Data', color = '#EBA911')
+    plt.scatter(x_test,y_test,label='Testing Data', color = '#0CC88F' )
     
     smooth_x = np.linspace(0,max(x_data),1000)
     
@@ -214,16 +216,30 @@ def magic(n, x_data = x_data, x_test = x_test, y_data = y_data, y_test = y_test)
     #print("Testing Error :", mseTest)
     return int(mseTrain), int(mseTest)
 
+def trainTestError(n):
+    error = []
+    for i in range(n):
+        error.append(magic(i+1))
+    
+    plt.plot(error)
+    plt.xticks(list(range(0,n)),list(range(1,n+1)))
+    
+    return error
 #%%
-magic(4)
-#%%
-smooth_x = np.linspace(0,max(x_data),1000)
-#%%
-error = []
-for i in range(10):
-    error.append(magic(i+1))
-error
 
-plt.plot(error)
 #%%
+trainTestError(10)
+
+#%%
+#Here we see that the testing error changes a lot according to the "shuffle" of the data, and changes the optimal degree of the polynome.
+#It would thus be clever to run a large nu;ber of simulations to find the "real" optimum (Monte Carlo simulation?) 
+
+
+
+
+
+
+
+
+
 
